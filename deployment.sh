1fb2a45d3b978c3d492c718e2034311f368b6c04
Altun Baratli <37396301+altunbaratli@users.noreply.github.com>
2020-11-26 23:59:58 +0100

    added new scripts to auto deploy and delete

---
+++ deployment.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd volume &&
+kubectl create -f createPVandPVC.yaml &&
+cd ../jobs &&
+kubectl apply -f copyArtifactsJob.yaml &&
+sleep 10 &&
+pod=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name}) &&
+kubectl cp ../artifacts $pod:/shared/
+kubectl apply -f generateCryptoConfig.yaml &&
+kubectl apply -f generateGenesisBlock.yaml &&
+kubectl apply -f generateChanneltx.yaml &&
+kubectl apply -f generateAnchorPeerMSPs.yaml &&
+cd ../network-deployment &&
+sh deployAll.sh &&
+sleep 10 &&
+cd ../jobs &&
+kubectl apply -f create_channel.yaml &&
+sleep 10 &&
+kubectl apply -f join_channel.yaml &&
+sleep 10 &&
+kubectl apply -f chaincode_install.yaml &&
+sleep 10 &&
+kubectl apply -f chaincode_instantaite.yaml &&
+sleep 10 &&
+kubectl apply -f updateAnchorPeers.yaml &&
+cd .. &&
+kubectl create deployment rest-api --image=altunbarat/rest-api &&
+kubectl expose deployment rest-api --port=3000 --target-port=3000 &&
+cd node-red &&
+kubectl create deployment node-red --image=yigitpolat/hyperledger-iot-nodered &&
+kubectl apply -f node-red-svc-nodePort.yaml &&
+kubectl get nodes -o wide
